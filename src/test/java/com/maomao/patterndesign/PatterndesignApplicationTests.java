9f96d7bd5cf5290a7823b942db77c75e461e5326
HatDu <1724021486@qq.com>
2022-06-13 20:18:10 +0800

    初次提交

---
+++ src/test/java/com/maomao/patterndesign/PatterndesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maomao.patterndesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatterndesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
